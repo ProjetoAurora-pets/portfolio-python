bd18ef4cb30ebc9274b1657a9c969afca65783e4
João Victor Viana Oliveira Silva (gaspar) <joaovctrjvv90@gmail.com>
2025-04-23 21:17:55 -0300

    codando os laços de repetição v.2

--- repeticao.py
+++ repeticao.py
@@ -1,2 +1,17 @@
-#CONCEITO E SIGNIFICADO DE 
+#CONCEITO E SIGNIFICADO DE LAÇO DE REPETIÇÃO
+##As linhas de codigo são executada repetidamente, enquanto uma condição for verdadeira ou até que uma condição específica seja atendida.
+
+#EXEMPLO DE FOR
+for i in range(1,11,2):
+    print(i)
+    ##ONDE "1" SERIA O STOP (começo da contagem) | '11' seria o final (sempre contando -1, pois inicia-se com o 0) | "2" seria o espaçamento, passos ou casas a ser pulada, no caso seria de 2 em 2. 
+    ## "i" seria o contador
+
+#EXEMPLO WHILE
+i = 1
+while i <= 5:
+    print(i)
+    i += 1
+## while é enquanto, sendo um laço de repetição que necessita de uma condição de parada para parar, se não continua repetindo a linha de codigo codada
+## onde 'i' é  contador e mais abaixo na linha de codigo adiciona o i+=1 pra acrescentar ou i-=1 para tirar (decrescente).:
 
