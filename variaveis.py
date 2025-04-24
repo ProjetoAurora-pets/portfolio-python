11a6ff4a88c337ef89c675499b4aa105fe174227
pedroReis <reisalcantaraph@gmail.com>
2025-04-23 21:31:11 -0300

    estou explicando variaveis e seus tipos

---
+++ variaveis.py
@@ -0,0 +1,25 @@
+# o que são variaveis ? 
+
+
+
+variavel = "espaço na memoria"
+
+
+
+#tipos de variaveis
+
+
+
+string = str ( "Armazena texto" )
+
+
+
+pontoFlutuante = float ( 3.141592653589793 ) 
+
+
+
+inteiro = int ( 40028922 )
+
+
+
+booleano = bool ( "true or false" )
